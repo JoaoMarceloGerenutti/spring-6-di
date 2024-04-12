7daa33ab5258b6f4cee8d52290ab4ba34c387592
Joao Marcelo <joaomarcelogerenuttii@hotmail.com>
2024-04-12 13:53:12 -0300

    Adding dep with Spring example

--- src/main/java/guru/springframework/spring6di/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/spring6di/controllers/ConstructorInjectedController.java
@@ -1,8 +1,11 @@
 package guru.springframework.spring6di.controllers;
 
 import guru.springframework.spring6di.services.GreetingService;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
+
     private final GreetingService greetingService;
 
     public ConstructorInjectedController(GreetingService greetingService) {
--- src/main/java/guru/springframework/spring6di/services/impl/GreetingServiceImpl.java
+++ src/main/java/guru/springframework/spring6di/services/impl/GreetingServiceImpl.java
@@ -1,8 +1,11 @@
 package guru.springframework.spring6di.services.impl;
 
 import guru.springframework.spring6di.services.GreetingService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class GreetingServiceImpl implements GreetingService {
+
     @Override
     public String sayGreeting() {
         return "Hello Everyone From Base Service!";
