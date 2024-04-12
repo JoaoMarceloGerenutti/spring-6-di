8de929105e0bd6bd994b63c2f0741ef8e81b07b9
Joao Marcelo <joaomarcelogerenuttii@hotmail.com>
2024-04-12 13:58:32 -0300

    Adding primary bean

---
+++ src/main/java/guru/springframework/spring6di/services/impl/GreetingServicePrimary.java
@@ -0,0 +1,14 @@
+package guru.springframework.spring6di.services.impl;
+
+import guru.springframework.spring6di.services.GreetingService;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Primary
+@Service
+public class GreetingServicePrimary implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hello From the Primary Bean!";
+    }
+}
