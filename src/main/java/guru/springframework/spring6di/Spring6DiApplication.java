55bed945805994706efc87bd7ccddfa930af6c69
Joao Marcelo <joaomarcelogerenuttii@hotmail.com>
2024-04-12 11:12:59 -0300

    Initialized Project

---
+++ src/main/java/guru/springframework/spring6di/Spring6DiApplication.java
@@ -0,0 +1,11 @@
+package guru.springframework.spring6di;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6DiApplication {
+
+	public static void main(String[] args) {
+	}
+
+}
