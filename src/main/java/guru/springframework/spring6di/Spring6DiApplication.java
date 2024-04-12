9a818e89c8d93b350b5a0b5f399401765f236edd
Joao Marcelo <joaomarcelogerenuttii@hotmail.com>
2024-04-12 11:15:57 -0300

    Adding context example

--- src/main/java/guru/springframework/spring6di/Spring6DiApplication.java
+++ src/main/java/guru/springframework/spring6di/Spring6DiApplication.java
@@ -1,11 +1,21 @@
 package guru.springframework.spring6di;
 
+import guru.springframework.spring6di.controller.MyController;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Spring6DiApplication {
 
 	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(Spring6DiApplication.class, args);
+
+		MyController controller = ctx.getBean(MyController.class);
+
+		System.out.println("In Main Methor");
+
+		System.out.println(controller.sayHello());
 	}
 
 }
